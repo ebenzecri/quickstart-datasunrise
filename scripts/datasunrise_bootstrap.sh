d1a81299f829ad1b77017c12809f03f32f1a834f
Eduardo Benzecri <e.benzecri@datasunrise.com>
2020-02-18 17:45:26 -0300

    Minor fixes

--- scripts/datasunrise_bootstrap.sh
+++ scripts/datasunrise_bootstrap.sh
@@ -179,7 +179,7 @@ installer_postinstall() {
 }
 
 installer_preinstall() {
-    yum install jq libtool-ltdl unixODBC -y
+    yum install java jq libtool-ltdl unixODBC -y
     wget -O /tmp/AmazonRedshiftODBC.rpm https://redshift-downloads.s3.amazonaws.com/drivers/odbc/1.4.8.1000/AmazonRedshiftODBC-64-bit-1.4.8.1000-1.x86_64.rpm
     yum --nogpgcheck localinstall /tmp/AmazonRedshiftODBC.rpm -y
 
